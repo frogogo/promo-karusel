f45f364e833e6acfc840b19e3a757a6575e620ba
Alexey Shatalin <xiaouending@ya.ru>
2020-04-15 00:21:53 +0300

    Remove extra library

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import { InlineSvgPlugin } from 'vue-inline-svg'
 import vueSmoothScroll from 'vue2-smooth-scroll'
-import VScrollLock from 'v-scroll-lock'
 import VueWindowSize from 'vue-window-size'
 
 require('./assets/tailwind.css');
@@ -11,7 +10,6 @@ Vue.config.productionTip = false
 
 Vue.use(InlineSvgPlugin)
 Vue.use(vueSmoothScroll)
-Vue.use(VScrollLock)
 Vue.use(VueWindowSize, { delay: 100 })
 
 new Vue({
