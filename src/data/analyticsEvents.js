56b5acdecc8ee0419b75e8e836697d350890c29e
Alexey Shatalin <xiaouending@ya.ru>
2020-04-28 13:58:00 +0300

    Add analytics events to data

---
+++ src/data/analyticsEvents.js
@@ -0,0 +1,44 @@
+export default {
+  howTo: {
+    event: 'karusel_promo',
+    eventAction: 'how_to',
+    eventCategory: 'karusel',
+    eventLabel: 'PageUrl'
+  },
+  collections: {
+    event: 'karusel_promo',
+    eventAction: 'collection',
+    eventCategory: 'karusel',
+    eventLabel: 'PageUrl'
+  },
+  participants: {
+    event: 'karusel_promo',
+    eventAction: 'shops',
+    eventCategory: 'karusel',
+    eventLabel: 'PageUrl'
+  },
+  rules: {
+    event: 'karusel_promo',
+    eventAction: 'rules',
+    eventCategory: 'karusel',
+    eventLabel: 'PageUrl'
+  },
+  booklet: {
+    event: 'karusel_promo',
+    eventAction: 'download',
+    eventCategory: 'karusel',
+    eventLabel: 'PageUrl'
+  },
+  sliderNavigationRight: {
+    event: 'karusel_promo',
+    eventAction: 'slider',
+    eventCategory: 'karusel',
+    eventLabel: 'Right'
+  },
+  sliderNavigationLeft: {
+    event: 'karusel_promo',
+    eventAction: 'slider',
+    eventCategory: 'karusel',
+    eventLabel: 'Left'
+  }
+}
